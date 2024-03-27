4b7b76176b3f45d37fc38f992384208f3b42e782
kalyanbhetwal <kalyanbtl@gmail.com>
2024-03-26 18:34:26 -0600

    not working i2c

--- memory.x
+++ memory.x
@@ -1,10 +1,11 @@
+STACK_SIZE = 4K;  
 MEMORY
 {
   /* NOTE 1 K = 1 KiBi = 1024 bytes */
   /* TODO Adjust these memory regions to match your device memory layout */
   /* These values correspond to the LM3S6965, one of the few devices QEMU can emulate */
-  FLASH : ORIGIN = 0x00000000, LENGTH = 256K
-  RAM : ORIGIN = 0x20000000, LENGTH = 64K
+  FLASH : ORIGIN = 0x08000000, LENGTH = 256K
+  RAM : ORIGIN = 0x20000000, LENGTH = 40K
 }
 
 /* This is where the call stack will be allocated. */
@@ -32,3 +33,11 @@ MEMORY
      } > RAM2
    } INSERT AFTER .bss;
 */
+
+/* Define the stack section */
+_estack = ORIGIN(RAM) + LENGTH(RAM);
+_stack_start = _estack;
+
+/* Specify the stack section location and size */
+PROVIDE(_stack_start = _stack_start);
+PROVIDE(_stack_end = _estack - STACK_SIZE);
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ use cortex_m_semihosting::{hprint, hprintln};
 
 use stm32f3xx_hal_v2::{self as hal, pac, prelude::*};
 
+const FRAM_ADDR: u8 = 0x50; // I2C device address
+const MEMORY_ADDRESS: u16 = 0x0001; // Address to write/read data to/from
+
 const VALID_ADDR_RANGE: Range<u8> = 0x08..0x78;
 
 #[entry]
@@ -36,7 +39,48 @@ fn main() -> ! {
     let mut i2c = hal::i2c::I2c::new(dp.I2C1, pins, 100.khz(), clocks, &mut rcc.apb1);
 
     hprintln!("Start i2c scanning...").expect("Error using hprintln.");
-    hprintln!().unwrap();
+   // hprintln!().unwrap();
+
+
+      // Data to write to F-RAM
+      let data_to_write: u8 = 0xAB;
+
+      // Write data to the specified memory location
+      let address_bytes: [u8; 2] = [(MEMORY_ADDRESS >> 8) as u8, MEMORY_ADDRESS as u8];
+    
+      i2c.write(FRAM_ADDR, &address_bytes).unwrap();
+      
+      match i2c.write(FRAM_ADDR, &[data_to_write]) {
+        Ok(_) => {
+            hprintln!("Success!!").unwrap();
+        }
+        Err(_) => {
+            hprintln!("Error!!").unwrap();
+        }
+    }
+
+        // Read data from the specified memory location
+     let mut data_read: [u8; 1] = [0];
+    match i2c.write_read(FRAM_ADDR, &address_bytes, &mut data_read){
+        Ok(_) => {
+            // Data read successfully
+            let data = data_read[0];
+            hprintln!("{}", data).unwrap();
+            // You can print or process the data as needed
+        }
+        Err(_) => {
+            hprintln!("Error reading data!!").unwrap();
+            // Error reading data
+        }
+    }
+
+    //hprintln!("{}", data_read[0]).unwrap();
+    hprintln!("Done!").unwrap();
+
+
+     // Write 8-bit value to the fixed address
+    // let data_to_write: [u8; 2] = [MEMORY_ADDRESS, 0xAB]; // Combine memory address and data
+
 
     for addr in 0x00_u8..0x80 {
         // Write the empty array and check the slave response.
@@ -52,8 +96,8 @@ fn main() -> ! {
         }
     }
 
-    hprintln!().unwrap();
-    hprintln!("Done!").unwrap();
+    // hprintln!().unwrap();
+    // hprintln!("Done!").unwrap();
 
     loop {
     
