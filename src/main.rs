6b7ab40ecfb46f8a2c9306d7bdb6967d5ac5697c
kalyanbhetwal <kalyanbtl@gmail.com>
2024-04-01 15:51:10 -0600

    i2c f-ram read complete

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,44 @@
 #![no_std]
 #![no_main]
 
-use core::ops::Range;
-
 use panic_halt as _;
-
-use cortex_m::asm;
+use core::ops::Range;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{hprint, hprintln};
 
-use stm32f3xx_hal_v2::{self as hal, pac, prelude::*};
+use stm32f3xx_hal_v2::{self as hal, pac, prelude::*, i2c::Error};
 
-const FRAM_ADDR: u8 = 0x50; // I2C device address
-const MEMORY_ADDRESS: u16 = 0x0001; // Address to write/read data to/from
+const VALID_ADDR_RANGE: Range<u8> = 0x40..0x55;
+const MEMORY_ADDRESS: u16 = 0x0000; // Address to write/read data to/from
+const DEVICE_ADDRESS: u8 = 0x50; // I2C device address
 
-const VALID_ADDR_RANGE: Range<u8> = 0x08..0x78;
+
+fn check_i2c_error(error: Error) -> Result<(), ()> {
+    match error {
+        Error::Arbitration => {
+            hprintln!("Arbitration loss occurred").unwrap();
+            Err(())
+        }
+        Error::Bus => {
+            hprintln!("Bus error occurred").unwrap();
+            Err(())
+        }
+        Error::Busy => {
+            hprintln!("Bus is busy").unwrap();
+            Err(())
+        }
+        Error::Nack => {
+            hprintln!("NACK received during transfer").unwrap();
+            Err(())
+        }
+        _ => {
+            hprintln!("Unknown error occurred").unwrap();
+            Err(())
+        }
+
+    }
+}
+
 
 #[entry]
 fn main() -> ! {
@@ -31,58 +55,19 @@ fn main() -> ! {
 
     let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
 
+    let  scl = gpiob.pb8.into_af4(&mut gpiob.moder, &mut gpiob.afrh); // SCL
+    let  sda = gpiob.pb9.into_af4(&mut gpiob.moder, &mut gpiob.afrh); // SDA
+
     // Configure I2C1
-    let pins = (
-        gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl), // SCL
-        gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl), // SDA
-    );
+    let pins = (scl, sda);
+
+    
     let mut i2c = hal::i2c::I2c::new(dp.I2C1, pins, 100.khz(), clocks, &mut rcc.apb1);
 
-    hprintln!("Start i2c scanning...").expect("Error using hprintln.");
-   // hprintln!().unwrap();
+    let fram_address = 0x50;
 
 
-      // Data to write to F-RAM
-      let data_to_write: u8 = 0xAB;
-
-      // Write data to the specified memory location
-      let address_bytes: [u8; 2] = [(MEMORY_ADDRESS >> 8) as u8, MEMORY_ADDRESS as u8];
-    
-      i2c.write(FRAM_ADDR, &address_bytes).unwrap();
-      
-      match i2c.write(FRAM_ADDR, &[data_to_write]) {
-        Ok(_) => {
-            hprintln!("Success!!").unwrap();
-        }
-        Err(_) => {
-            hprintln!("Error!!").unwrap();
-        }
-    }
-
-        // Read data from the specified memory location
-     let mut data_read: [u8; 1] = [0];
-    match i2c.write_read(FRAM_ADDR, &address_bytes, &mut data_read){
-        Ok(_) => {
-            // Data read successfully
-            let data = data_read[0];
-            hprintln!("{}", data).unwrap();
-            // You can print or process the data as needed
-        }
-        Err(_) => {
-            hprintln!("Error reading data!!").unwrap();
-            // Error reading data
-        }
-    }
-
-    //hprintln!("{}", data_read[0]).unwrap();
-    hprintln!("Done!").unwrap();
-
-
-     // Write 8-bit value to the fixed address
-    // let data_to_write: [u8; 2] = [MEMORY_ADDRESS, 0xAB]; // Combine memory address and data
-
-
-    for addr in 0x00_u8..0x80 {
+    for addr in 0x48_u8..0x52 {
         // Write the empty array and check the slave response.
         if VALID_ADDR_RANGE.contains(&addr) && i2c.write(addr, &[]).is_ok() {
             hprint!("{:02x}", addr).unwrap();
@@ -95,11 +80,23 @@ fn main() -> ! {
             hprint!(" ").unwrap();
         }
     }
+     // Read data from the memory location
+     let memory_address_bytes = [(MEMORY_ADDRESS >> 8) as u8, MEMORY_ADDRESS as u8];
 
-    // hprintln!().unwrap();
-    // hprintln!("Done!").unwrap();
-
+    let mut data = [0;100];
+    i2c.write_read(fram_address, &memory_address_bytes, &mut data).unwrap();
+    hprintln!("Data read: {:?}", data).unwrap();
+    //{
+    //     Ok(()) => {
+    //         //let data =  data[0];
+    //         hprintln!("Data read: {:?}", data).unwrap();
+    //     }
+    //     Err(e) => {
+    //         check_i2c_error(e).unwrap();
+    //         loop {} // Exit if error occurs
+    //     }
+    // }
     loop {
-    
+       
     }
 }
