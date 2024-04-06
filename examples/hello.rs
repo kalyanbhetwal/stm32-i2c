c592d4fe4280e2f457f568fe15d7923b9c9b8afb
kalyanbhetwal <kalyanbtl@gmail.com>
2024-04-06 10:31:08 -0600

    i2c with timings for read and write

--- examples/hello.rs
+++ examples/hello.rs
@@ -8,13 +8,27 @@ use panic_halt as _;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{debug, hprintln};
 
+
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
 
+    if let Some(dp) = Peripherals::take() {
+        // RCC (Reset and Clock Control) peripheral
+        let rcc = dp.RCC.constrain();
+
+        // Clocks
+        let clocks = rcc.cfgr.freeze();
+
+        // Get the system clock frequency
+        let sysclk = clocks.sysclk().0;
+
+        // Print the system clock frequency
+        defmt::info!("System Clock Frequency: {} Hz", sysclk);
+
+    }
     // exit QEMU
     // NOTE do not run this on hardware; it can corrupt OpenOCD state
-   // debug::exit(debug::EXIT_SUCCESS);
+   debug::exit(debug::EXIT_SUCCESS);
 
     loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,15 @@ use core::ops::Range;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::{hprint, hprintln};
 
-use stm32f3xx_hal_v2::{self as hal, pac, prelude::*, i2c::Error};
+use cortex_m::peripheral::{Peripherals, DWT};
+
+use stm32f3xx_hal_v2::{self as hal, pac, prelude::*};
 
 const VALID_ADDR_RANGE: Range<u8> = 0x40..0x55;
 const MEMORY_ADDRESS: u16 = 0x0000; // Address to write/read data to/from
 const MEMORY_ADDRESS_2: u16 = 0x0001; // Address to write/read data to/from
 
-
-fn check_i2c_error(error: Error) -> Result<(), ()> {
-    match error {
-        Error::Arbitration => {
-            hprintln!("Arbitration loss occurred").unwrap();
-            Err(())
-        }
-        Error::Bus => {
-            hprintln!("Bus error occurred").unwrap();
-            Err(())
-        }
-        Error::Busy => {
-            hprintln!("Bus is busy").unwrap();
-            Err(())
-        }
-        Error::Nack => {
-            hprintln!("NACK received during transfer").unwrap();
-            Err(())
-        }
-        _ => {
-            hprintln!("Unknown error occurred").unwrap();
-            Err(())
-        }
-
-    }
-}
+const CLOCK_FREQUENCY: u32 = 8_000_000; // Clock frequency in Hz
 
 
 #[entry]
@@ -60,39 +37,43 @@ fn main() -> ! {
 
     // Configure I2C1
     let pins = (scl, sda);
-
-    
     let mut i2c = hal::i2c::I2c::new(dp.I2C1, pins, 100.khz(), clocks, &mut rcc.apb1);
 
     let fram_address = 0x50;
 
+    let  core_peripherals = Peripherals::take().unwrap();
+    let mut dwt = core_peripherals.DWT;
+    dwt.enable_cycle_counter();
 
-    for addr in 0x48_u8..0x52 {
-        // Write the empty array and check the slave response.
-        if VALID_ADDR_RANGE.contains(&addr) && i2c.write(addr, &[]).is_ok() {
-            hprint!("{:02x}", addr).unwrap();
-        } else {
-            hprint!("..").unwrap();
-        }
-        if addr % 0x10 == 0x0F {
-            hprintln!().unwrap();
-        } else {
-            hprint!(" ").unwrap();
-        }
-    }
-     // Read data from the memory location
-     let memory_address_bytes = [(MEMORY_ADDRESS >> 8) as u8, MEMORY_ADDRESS as u8];
-     let value = 0x0C;
+    // for addr in 0x48_u8..0x52 {
+    //     // Write the empty array and check the slave response.
+    //     if VALID_ADDR_RANGE.contains(&addr) && i2c.write(addr, &[]).is_ok() {
+    //         hprint!("{:02x}", addr).unwrap();
+    //     } else {
+    //         hprint!("..").unwrap();
+    //     }
+    //     if addr % 0x10 == 0x0F {
+    //         hprintln!().unwrap();
+    //     } else {
+    //         hprint!(" ").unwrap();
+    //     }
+    // }
+
+
+    let value = 0x0C;
      let buff = [
         ((MEMORY_ADDRESS >> 8) & 0xFF) as u8,
         (MEMORY_ADDRESS & 0xFF) as u8,
         value
     ];
-
-
-
     // Write the buffer to the I2C device
-    i2c.write(fram_address, &buff).unwrap();
+    
+   let start_write = DWT::cycle_count();
+   i2c.write(fram_address, &buff).unwrap();
+   let end_write = DWT::cycle_count();
+   let write_time = ((end_write - start_write) as f32 / CLOCK_FREQUENCY as f32) * 1000.0;
+
+   hprintln!("Write time: {} ms", write_time).unwrap();
 
     let value = 0x0A;
     let buff = [
@@ -101,11 +82,31 @@ fn main() -> ! {
        value
    ];
 
+   let start_write = DWT::cycle_count();
    i2c.write(fram_address, &buff).unwrap();
+   let end_write = DWT::cycle_count();
+   let write_time = ((end_write - start_write) as f32 / CLOCK_FREQUENCY as f32) * 1000.0;
+
+   hprintln!("Write time: {} ms", write_time).unwrap();
 
 
-    let mut data = [0;100];
-    i2c.write_read(fram_address, &memory_address_bytes, &mut data).unwrap();
+   let mut data = [0;1];
+   let memory_address_bytes = [(MEMORY_ADDRESS >> 8) as u8, MEMORY_ADDRESS as u8];
+   let start_read = DWT::cycle_count();
+   i2c.write_read(fram_address, &memory_address_bytes, &mut data).unwrap();
+   let end_read = DWT::cycle_count();
+   let read_time = ((end_read - start_read) as f32 / CLOCK_FREQUENCY as f32) * 1000.0;
+   hprintln!("Read time: {} ms", read_time).unwrap();
+   hprintln!("Data read: {:?}", data).unwrap();
+
+   let mut data = [0;1];
+   let memory_address_bytes = [(MEMORY_ADDRESS_2 >> 8) as u8, MEMORY_ADDRESS_2 as u8];
+   let start_read = DWT::cycle_count();
+   i2c.write_read(fram_address, &memory_address_bytes, &mut data).unwrap();
+   let end_read = DWT::cycle_count();
+   let read_time = ((end_read - start_read) as f32 / CLOCK_FREQUENCY as f32) * 1000.0;
+
+    hprintln!("Read time: {} ms", read_time).unwrap();
     hprintln!("Data read: {:?}", data).unwrap();
     //{
     //     Ok(()) => {
